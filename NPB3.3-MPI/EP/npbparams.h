145036025f654f4f5a3aeb153f25456b50801a52
filipecosta90 <filipecosta.90@gmail.com>
2016-02-25 11:13:04 +0000

    added OMP and SER final results

---
+++ NPB3.3-MPI/EP/npbparams.h
@@ -0,0 +1,31 @@
+c NPROCS = 16 CLASS = C
+c  
+c  
+c  This file is generated automatically by the setparams utility.
+c  It sets the number of processors and the class of the NPB
+c  in this directory. Do not modify it by hand.
+c  
+        character class
+        parameter (class ='C')
+        integer m, npm
+        parameter (m=32, npm=16)
+        logical  convertdouble
+        parameter (convertdouble = .false.)
+        character*11 compiletime
+        parameter (compiletime='23 Feb 2016')
+        character*5 npbversion
+        parameter (npbversion='3.3.1')
+        character*6 cs1
+        parameter (cs1='mpif77')
+        character*9 cs2
+        parameter (cs2='$(MPIF77)')
+        character*22 cs3
+        parameter (cs3='-L/usr/local/lib -lmpi')
+        character*20 cs4
+        parameter (cs4='-I/usr/local/include')
+        character*6 cs5
+        parameter (cs5='-O -O3')
+        character*6 cs6
+        parameter (cs6='-O -O3')
+        character*6 cs7
+        parameter (cs7='randi8')
